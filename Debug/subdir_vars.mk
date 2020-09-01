dd64f6a4260361ce80204a5746010eac1c9c9ff0
AvaRat <marcelos.kalinski@gmail.com>
2020-09-01 11:26:16 +0200

    initial commit

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,53 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr6989.cmd 
+
+C_SRCS += \
+../StopWatchMode.c \
+../TempSensorMode.c \
+../app_logic.c \
+../hal_LCD.c \
+../main.c 
+
+C_DEPS += \
+./StopWatchMode.d \
+./TempSensorMode.d \
+./app_logic.d \
+./hal_LCD.d \
+./main.d 
+
+OBJS += \
+./StopWatchMode.obj \
+./TempSensorMode.obj \
+./app_logic.obj \
+./hal_LCD.obj \
+./main.obj 
+
+OBJS__QUOTED += \
+"StopWatchMode.obj" \
+"TempSensorMode.obj" \
+"app_logic.obj" \
+"hal_LCD.obj" \
+"main.obj" 
+
+C_DEPS__QUOTED += \
+"StopWatchMode.d" \
+"TempSensorMode.d" \
+"app_logic.d" \
+"hal_LCD.d" \
+"main.d" 
+
+C_SRCS__QUOTED += \
+"../StopWatchMode.c" \
+"../TempSensorMode.c" \
+"../app_logic.c" \
+"../hal_LCD.c" \
+"../main.c" 
+
+
